462cca7a21befdfe516f9949a006659cfd75f46d
Leandro Carvalho <carvalho.inacio@gmail.com>
2016-11-30 23:34:53 -0300

    Improving ssh.py hardening check

--- HnTool/modules/ssh.py
+++ HnTool/modules/ssh.py
@@ -51,15 +51,6 @@ class Rule(MasterRule):
                 else:
                     check_results['low'].append('SSH is using the default port')
 
-                # Checking if the Root Login is allowed
-                if 'PermitRootLogin' in lines:
-                    if lines['PermitRootLogin'] == 'yes':
-                        check_results['medium'].append('Root access allowed')
-                    else:
-                        check_results['ok'].append('Root access is not allowed')
-                else:
-                    check_results['medium'].append('Root access is allowed')
-
                 # Checking if SSH is using protocol v2 (recommended)
                 if 'Protocol' in lines:
                     if int(lines['Protocol']) == 2:
@@ -69,6 +60,60 @@ class Rule(MasterRule):
                 else:
                     check_results['high'].append('SSH is not using protocol v2')
 
+                # Checking if the Root Login is allowed
+                if 'PermitRootLogin' in lines:
+                    if lines['PermitRootLogin'] == 'yes':
+                        check_results['medium'].append('Root access allowed')
+                    else:
+                        check_results['ok'].append('Root access is not allowed')
+                else:
+                    check_results['medium'].append('Root access is allowed')
+
+                # Disconnects no successfully login after 30 seconds
+                if 'LoginGraceTime' in lines:
+                    if int(lines['LoginGrace']) >= 30:
+                        check_results['low'].append('Keeping connection login attempt to more than 30 seconds')
+                    else:
+                        check_results['ok'].append('Disconnects no successfully login after 30 seconds')
+                else:
+                    check_results['low'].append('Keeping connection login attempt to more than 30 seconds')
+
+                # Disconnects session after more than 120 seconds of inactivity 
+                if 'ClientAliveInterval' in lines and int(lines['Protocol']) == 2:
+                    if int(lines['ClientAliveInterval']) >= 120:
+                        check_results['low'].append('Keeping inactive session')
+                    else:
+                        check_results['ok'].append('Disconnects session after more than 120 seconds of inactivity')
+                else:
+                    check_results['low'].append('Keeping inactive session')
+                    
+                # Total number of checkalive message sent by the ssh server
+                if 'ClientAliveCountMax' in lines and int(lines['Protocol']) == 2:
+                    if int(lines['ClientAliveInterval']) >= 4:
+                        check_results['low'].append('High total number of checkalive message')
+                    else:
+                        check_results['ok'].append('Great value checkalive message')
+                else:
+                    check_results['low'].append('High total number of checkalive message')
+
+                # Number of tries to login
+                if 'MaxAuthTries' in lines:
+                    if int(lines['MaxAuthTries']) >= 5:
+                        check_results['low'].append('High value for login attempts')
+                    else:
+                        check_results['ok'].append('Good value for login attempts')
+                else:
+                    check_results['low'].append('High value for login attempts')
+
+                # Total number of open sessions
+                if 'MaxSessions' in lines:
+                    if int(lines['MaxSessions']) >= 4:
+                        check_results['low'].append('High number of open sessions allowed')
+                    else:
+                        check_results['ok'].append('Good value of open sessions allowed')
+                else:
+                    check_results['low'].append('High number of open sessions allowed')
+
                 # Checking if empty password are allowed (shouldn't)
                 if 'PermitEmptyPasswords' in lines:
                     if lines['PermitEmptyPasswords'] == 'yes':
@@ -77,6 +122,15 @@ class Rule(MasterRule):
                         check_results['ok'].append('Empty passwords are not allowed')
                 else:
                     check_results['high'].append('Empty passwords are allowed')
+
+                # Specifies whether rhosts or shosts files should not be used in authentication
+                if 'IgnoreRhosts' in lines:
+                    if lines['IgnoreRhosts'] == 'yes':
+                        check_results['high'].append('Using rhosts file')
+                    else:
+                        check_results['ok'].append('Not using rhosts file')
+                else:
+                    check_results['high'].append('Using rhosts file')
 
                 # Checking if X11 Forward is allowed (shouldn't)
                 if 'X11Forwarding' in lines:
